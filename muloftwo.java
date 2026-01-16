3172885912504fdb90415ab528aecb842f43d12e
devanshi19b <devanshibansal44@gmail.com>
2026-01-16 11:31:31 +0530

    1COMMIT

---
+++ muloftwo.java
@@ -0,0 +1,10 @@
+public class muloftwo {
+    public static void main(String[] args){
+        int n=10;
+        for (int i = 1; i <= n; i++) {
+            int result =i * 2;
+            System.out.println(i + " * 2 = " + result);
+    }
+    
+}
+}
---
+++ print.java
@@ -0,0 +1,8 @@
+public class print {
+    public static void main(String[] args) {
+        System.out.println("Name: Devanshi Bansal");
+        System.out.println("Age: 21");
+        System.out.println("College name: GLA UNIVERSITY");
+    }
+    
+}
